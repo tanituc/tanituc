1ce24c5f2d0fd93256cc14ab681106d2730070ad
Estanisla Cordoba <tanituc@gmail.com>
2024-12-31 22:21:34 -0300

    first commit

---
+++ tanituc.client/src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+
+export default function Home() {
+  return (
+    <Box>
+      <header>hamburger button</header>
+      <Box>
+        <Box>leftSection</Box>
+        <Box>rightSection</Box>
+      </Box>
+      <footer>some text</footer>
+    </Box>
+  );
+}
